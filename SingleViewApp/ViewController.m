9cc308cf5c3b6a37bb53efa866b9306d65e0245f
samaygrover <samaygrover@gmail.com>
2014-05-12 21:25:50 -0400

    //third Commit with auto focus

--- SingleViewApp/ViewController.m
+++ SingleViewApp/ViewController.m
@@ -19,26 +19,27 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     UIView* mainView=self.view;
-    UIView* v=[[UIView alloc]initWithFrame:CGRectMake(115,111,132,194)];
-    UIView* v2=[[UIView alloc]initWithFrame:CGRectMake(43,111,120,111)];
-    UIView* v3=[[UIView alloc]initWithFrame:CGRectInset(v2.bounds, 17, 15)];
+    UIView* v=[[UIView alloc]initWithFrame:CGRectMake(100,111,132,194)];
+    UIView* v2=[[UIView alloc]initWithFrame:CGRectMake(0,0,132,10)];
+    UIView* v3=[[UIView alloc]initWithFrame:CGRectMake(v.bounds.size.width-17,v.bounds.size.height-20,20,20)];
     
     v.backgroundColor=[UIColor colorWithRed:1 green:.4 blue:1 alpha:1];
     v2.backgroundColor=[UIColor colorWithRed:.5 green:.4 blue:.7 alpha:1];
     v3.backgroundColor=[UIColor colorWithRed:.7 green:.4 blue:0 alpha:1];
     
-    v2.transform=CGAffineTransformMakeRotation(45*M_PI/180);
-    
+   // v2.transform=CGAffineTransformMakeRotation(45*M_PI/180);
+    v2.autoresizingMask=UIViewAutoresizingFlexibleWidth;
+    v3.autoresizingMask=UIViewAutoresizingFlexibleTopMargin| UIViewAutoresizingFlexibleLeftMargin;
     [mainView addSubview:v];
     [v addSubview:v2];
     [v2 addSubview:v3];//added by Ullas Joseph
     
-   /* CGRect r =v2.bounds;
-    r.size.height+=100;
-    r.size.width-=10;
-    r.origin.x+=100;
-    v2.bounbs
-    */
+    CGRect r =v2.bounds;
+    r.size.height+=39;
+    r.size.width-=50;
+    v.bounds=r;
+    
+    
     mainView.backgroundColor=[UIColor yellowColor];
     
 }
